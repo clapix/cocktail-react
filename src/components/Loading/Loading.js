e61e0b041dadbca598d1d3521dda056e1a4b3485
Charlotte Drew <ckdrew670@hotmail.com>
2019-12-18 15:09:36 +0000

    Added GET request and Loading component to Cocktails Archive page

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+
+
 class Loading extends Component {
     componentDidMount() {
         if (!this.props.loaded) {
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,12 +1,14 @@
 import { connect } from "react-redux";
 import Loading from "./Loading";
+import { getCocktails } from "../../data/actions/api";
+
 
 const mapStateToProps = ({ loaded }) => ({
     loaded,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    handleLoad: () => dispatch(getList()),
+    handleLoad: () => dispatch(getCocktails()),
 });
 
 
