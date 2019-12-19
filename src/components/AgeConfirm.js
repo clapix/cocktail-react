154f388effe9219ec679dedb5e8a16cda09ee71f
Charlotte Drew <ckdrew670@hotmail.com>
2019-12-19 10:43:00 +0000

    Added AgeConfirm Component

---
+++ src/components/AgeConfirm.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Form, Button, Container } from "react-bootstrap";
+
+class AgeConfirm extends Component {
+    constructor(props) {
+        super(props);
+        this.handleAgeConfirm = this.handleAgeConfirm.bind(this);
+    }
+
+    handleAgeConfirm(e) {
+    
+        e.preventDefault();
+        this.setState({ displayAgeConfirm: false });
+    
+    }
+    render() {
+        return (
+
+            <Container>
+                <h2>Please confirm that you are over the legal drinking age for your country of residence.</h2>
+                <Form>
+                    <Button 
+                    type="submit" 
+                    variant="btn btn-danger"
+                    >Confirm</Button>
+                </Form>
+            </Container>
+        )
+    };
+}
+
+export default AgeConfirm;
