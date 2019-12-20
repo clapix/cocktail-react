d8456b7c8bc77ac1e082c76615537cc4a85b2492
Eleanor <e.e.j@live.co.uk>
2019-12-20 15:06:21 +0000

    Updates

---
+++ precache-manifest.a47a0e43f76e11cc7d2dd8b91ad31ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fe9641154a5c965f2e2cabe1786037",
+    "url": "/cocktail-react/index.html"
+  },
+  {
+    "revision": "7141ff66da345a9f989c",
+    "url": "/cocktail-react/static/css/main.df9bdab7.chunk.css"
+  },
+  {
+    "revision": "405b9bd7cdd436c56999",
+    "url": "/cocktail-react/static/js/2.382ff286.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/cocktail-react/static/js/2.382ff286.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7141ff66da345a9f989c",
+    "url": "/cocktail-react/static/js/main.49332744.chunk.js"
+  },
+  {
+    "revision": "969f28dfafe3b3e63d6d",
+    "url": "/cocktail-react/static/js/runtime-main.912081fe.js"
+  }
+]);
