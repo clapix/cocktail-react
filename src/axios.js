27727903c0af6345b95a9e346b49ef30bdef3352
Eleanor <e.e.j@live.co.uk>
2019-12-20 16:23:19 +0000

    fixes https issue

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // return a new version of axios with useful settings applied
 export default axios.create({
-    baseURL: "http://tuesday.developme.space/api",
+    baseURL: "https://tuesday.developme.space/api",
     // params: {
     //     key: "b1a4b1a4b1a4-api-key-b1a4b1a4ab1a4",
     // },
